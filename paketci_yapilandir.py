c07eec36170d897b3f05215aa586e834744efe0c
Alp Erbil <beratalp@gmail.com>
2015-10-30 13:30:23 +0300

    Configuration system has translated (2.2)

--- paketci_yapilandir.py
+++ paketci_yapilandir.py
@@ -11,7 +11,7 @@ def start():
 	paket3 = "3)yum"
 	paket4 = "4)Pacman"
 	paket5 = "5)SuSe (zypper)"
-	paket6 = "6)Diğer"
+	paket6 = "6)Other"
 	os.chdir("/usr/share/paketci")
 	yap = open("yapilandir.txt", "w")
 	kur = open("kur.txt", "w")
@@ -19,34 +19,33 @@ def start():
 	print paket2
 	print paket3
 	print paket4
-	print "Lütfen paket yöneticinizi seçiniz"
-	yapsoru = raw_input("Paket yöneticinizin numarası: ")
+	yapsoru = raw_input("Enter the number for your package managar: ")
 	if yapsoru == "1":
-		print "Seçtiğiniz paket yöneticisi PiSi(Pardus)"
+		print "The chosen package manager is PiSi (Pardus)"
 		yap.write("pisi sr")
 		kur.write("pisi it")
 	elif yapsoru == "2":
-		print "Seçtiğiniz paket yöneticisi dpkg"
+		print "You've chosen dpkg (Ubuntu - Debian)"
 		yap.write("aptitude search")
 		kur.write("apt-get install")
 	elif yapsoru == "3":
-		print "Seçtiğiniz paket yöneticisi Yum"
+		print "You've choesen Yum"
 		yap.write("yum search")
 		kur.write("yum install")
 	elif yapsoru == "4":
-		print "Seçtiğiniz paket yöneticisi Pacman(ArchLinux)"
+		print "You've chosen Pacman (ArchLinux)"
 		yap.write("pacman -Ss")
 		kur.write("pacman -S")
 	elif yapsoru == "5":
-                print "Seçtiğiniz paket yöneticisi Zypper(SuSe)"
+                print "You've chosen zypper (SuSe)"
                 yap.write("zypper search")
                 kur.write("zypper install")
 	
 	elif yapsoru == "6":
-		print "Elle yapılandırmayı seçtiniz"
-		yonetici = raw_input("Paket yöneticiniz ile arama yaptığınız komutu giriniz(Örneğin: 'pisi sr'): ")
+		print "Manual configuration is starting..."
+		yonetici = raw_input("What is the search command of your package manager: ")
 		yap.write("%s" %(yonetici))
-		kurucu = raw_input("Paket yöneticiniz ile kurulum yaptığınız komutu giriniz: ")
+		kurucu = raw_input("What is the installation command of your package manager: ")
 		kur.write("%s" %(kurucu))
 	else:
-		print "Yapılandırma kullanıcı tarafından iptal edildi!"
+		print "Error. Configuration aborted."
