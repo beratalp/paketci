054d0898e83236e849692c6f8255175df0757be4
Alp Erbil <beratalp@gmail.com>
2015-10-30 13:32:08 +0300

    Removal system has translated and made simple.

--- paketci_kaldir.py
+++ paketci_kaldir.py
@@ -4,11 +4,7 @@ import os
 import sys
 
 def start():
-	print "Paketçi kaldırması başlatılıyor"
-	#kaldir = open("~/.paketci.txt","w")
-	print "Lütfen kaldıracağınız paketleri tek tek kaldırınız."
-	os.system("paketci debian kaldir")
-	print  "Kaldırma bitti."
+	print "Removing paketci..."
 	os.system("rm -rf /usr/share/paketci")
 	os.system("rm -rf /usr/bin/paketci")
-	print "Paketçi başarıyla kaldırıldı."
+	print "Paketci has removed from your system."
